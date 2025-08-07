be1e03e635eef8c053f6cefd21debacbd2f096a8
CherteelA <asitnikov770@gmail.com>
2025-08-07 21:12:44 +0300

    Поменял симметричный ключ на несимметричный

--- src/main/java/com/myshop/userservice/service/JwtService.java
+++ src/main/java/com/myshop/userservice/service/JwtService.java
@@ -4,13 +4,21 @@ package com.myshop.userservice.service;
 import com.myshop.userservice.repository.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
+import java.io.InputStream;
+
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import java.util.Date;
 import java.util.UUID;
 
@@ -18,18 +26,71 @@ import java.util.UUID;
 @Service
 public class JwtService {
 
-    @Value("${app.jwt.secret}")
-    private String jwtSecret;
+
 
     @Value("${app.jwt.expiration}")
     private long jwtExpirationMs; // например, 900000 = 15 минут
 
-    private final Key key;
+    private final ResourceLoader resourceLoader;
 
-    // Конструктор для инъекции ключа
-    public JwtService(@Value("${app.jwt.secret}") String secret) {
-        byte[] keyBytes = Decoders.BASE64.decode(secret);
-        this.key = Keys.hmacShaKeyFor(keyBytes);
+
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+
+    public JwtService(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+        try {
+
+            privateKey = this.loadPrivateKey();
+            publicKey = this.loadPublicKey();
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private PrivateKey loadPrivateKey() throws Exception {
+        Resource resource = resourceLoader.getResource("classpath:security/private.pem");
+
+        try {
+            InputStream is = resource.getInputStream();
+            String privateKeyPEM = new String(is.readAllBytes())
+                    .replace("-----BEGIN PRIVATE KEY-----", "")
+                    .replace("-----END PRIVATE KEY-----", "")
+                    .replaceAll("\\s", "");
+            byte[] decoded = Base64.getDecoder().decode(privateKeyPEM);
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decoded);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return keyFactory.generatePrivate(keySpec);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private PublicKey loadPublicKey() throws Exception {
+
+        Resource resource = resourceLoader.getResource("classpath:security/public.pem");
+
+        try {
+            InputStream is = resource.getInputStream();
+
+
+            String publicKeyPEM = new String(is.readAllBytes())
+                    .replace("-----BEGIN PUBLIC KEY-----", "")
+                    .replace("-----END PUBLIC KEY-----", "")
+                    .replaceAll("\\s", "");
+
+            byte[] decoded = Base64.getDecoder().decode(publicKeyPEM);
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decoded);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return keyFactory.generatePublic(keySpec);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
@@ -39,13 +100,13 @@ public class JwtService {
                 .claim("role", "USER")
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
-                .signWith(key, SignatureAlgorithm.HS256)
+                .signWith(privateKey, SignatureAlgorithm.RS256)
                 .compact();
     }
 
     public UUID getUserIdFromToken(String token) {
         String subject = Jwts.parserBuilder()
-                .setSigningKey(key)
+                .setSigningKey(publicKey)
                 .build()
                 .parseClaimsJws(token)
                 .getBody()
@@ -66,7 +127,7 @@ public class JwtService {
 
     private Date getExpiration(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(key)
+                .setSigningKey(publicKey)
                 .build()
                 .parseClaimsJws(token)
                 .getBody()
